191b3091a5ca58927fbdd752616aff68ea71e833
JuanSaavedra <juan.ps.a@hotmail.com>
2025-01-09 14:51:50 -0400

    + add: mobile responsiveness, store theme preference

--- public/js/themeSelector.js
+++ public/js/themeSelector.js
@@ -1,13 +1,51 @@
-function toggleDarkMode() {
-    const html = document.documentElement; // Get the <html> element
-    
-    // If dark mode is currently on, turn it off, and vice versa
-    if (html.getAttribute('data-theme') === 'dark') {
-        html.removeAttribute('data-theme');
-    } else {
-        html.setAttribute('data-theme', 'dark');
+function loadTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.documentElement.setAttribute('data-theme', savedTheme);
     }
 }
 
-// Connect this to your theme toggle button
-document.querySelector('.theme-switch').addEventListener('change', toggleDarkMode);
+  // Función para alternar el tema
+function toggleTheme() {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+
+    // Guardar el tema en localStorage
+    localStorage.setItem('theme', newTheme);
+}
+
+  // Cargar el tema al cargar la página
+loadTheme();
+
+  // Agregar evento al botón para cambiar el tema
+document.querySelector('.theme-switch').addEventListener('change', toggleTheme);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menuItems = document.querySelector('.menu-items');
+    
+    menuToggle.addEventListener('click', function() {
+        menuToggle.classList.toggle('active');
+        menuItems.classList.toggle('active');
+    });
+
+    // Close menu when clicking outside
+    document.addEventListener('click', function(e) {
+        if (!menuToggle.contains(e.target) && !menuItems.contains(e.target)) {
+            menuToggle.classList.remove('active');
+            menuItems.classList.remove('active');
+        }
+    });
+});
+
+document.querySelector('.mission').addEventListener('click', function () {
+    const mtextElement = this.querySelector('.mission__description');
+    mtextElement.classList.toggle('expanded');
+});
+
+document.querySelector('.vision').addEventListener('click', function () {
+    const vtextElement = this.querySelector('.vision__description');
+    vtextElement.classList.toggle('expanded');
+});
