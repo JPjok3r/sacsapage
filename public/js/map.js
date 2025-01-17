ecaf325f78858f44da7dd55a82e4f11d997f17fa
JuanSaavedra <juan.ps.a@hotmail.com>
2025-01-17 15:14:25 -0400

    * fix: clean files not used, and unnecesary comments

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
     // Coordenadas de la ubicación (ejemplo: Ciudad de México)
     const location = { lat: -17.376804, lng: -66.163519 };
-
+    
     // Crear el mapa
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 17, // Nivel de zoom
--- public/js/separatorLinesFixes.js
+++
@@ -1,9 +0,0 @@
-StyleFix.register(function(css, raw) {
-	if (PrefixFree.functions.indexOf('radial-gradient') > -1) {
-		css = css.replace(/radial-gradient\(([a-z-\s]+\s+)?at ([^,]+)(?=,)/g, function($0, shape, center){
-			return 'radial-gradient(' + center + (shape? ', ' + shape : '');
-		});
-	}
-
-	return css;
-});
